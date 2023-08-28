3d3ce7970960137483b6802cf1c7e53a3a4be642
Λ <87445503+01000001khan@users.noreply.github.com>
2023-08-28 19:58:09 -0400

    hmmm

--- demo.js
+++ demo.js
@@ -2,19 +2,21 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 import { RGBELoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/RGBELoader.js';
 
+// Three.js stuff
+const loader = new GLTFLoader();
 let scene = new THREE.Scene();
 let core = new THREE.Mesh();
+let renderer = new THREE.WebGLRenderer({antialias:true});
 scene.add(core);
 
-const loader = new GLTFLoader();
+
+// My non-critical variables (except camera. if you remove that, brace for errors)
 let dt = 0;
 let time = 0;
-
-
-
+let inposition = false;
 let camera = null;
+const slider = document.getElementById("slider");
 
-let renderer = new THREE.WebGLRenderer({antialias:true});
 
 renderer.setClearColor("#000");
 
@@ -35,26 +37,25 @@ document.body.appendChild( renderer.domElement );
 // LIGHT ////////////////
 
 new RGBELoader().load( './assets/textures/leadenhall.hdr', function ( texture ) {
+
     texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = texture;    
+    scene.environment = texture;
+
 });
 
 let speaker = null;
 loader.load( './assets/models/decor/decorC1 render quality.glb', function ( gltf ) {
+
     speaker = gltf.scene;
 	scene.add( speaker );
 
-
-    console.log(gltf.camera)
-    console.log(gltf.cameras)
     camera = gltf.cameras[0];
 
 },undefined,function(error){console.error(error);});
 
 
-const slider = document.getElementById("slider");
-let time = 0;
-// Render Loop
+
+// Recursive render Loop
 function render(t) {
 
     let newframe = true;
@@ -66,13 +67,21 @@ function render(t) {
         speaker.position.z = Math.sin(time) // test animation
     }
 
-    requestAnimationFrame( render );
 
-    // The actual render call
-    if (newframe // stop rendering every frame to save performance when not moving
-        && camera){ // doubles as a check to make sure the model's actually loaded
 
-        renderer.render(scene, camera);
+    requestAnimationFrame( render );    // Request the next frame before we're actually rendered
+                                        // this one because js is weird and everything runs async
+
+    
+    if (newframe                        // stop rendering every frame to save performance when not moving
+        && camera){                     // doubles as a check to make sure the model's actually loaded
+
+        renderer.render(scene, camera); // The actual render call
+
+        if (!inposition){
+            document.getElementById("stuff").append(document.querySelector("canvas"));
+            inposition = true;
+        }
     }
 }
 
@@ -90,4 +99,7 @@ addEventListener("resize", (e) => {
 });
 
 
-document.getElementById("stuff").append(document.querySelector("canvas"));
+
+
+// Wuzzis do?
+console.log(this);
