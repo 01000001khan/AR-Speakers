767795bce189b1c094180264481638242a05b911
Λ <87445503+01000001khan@users.noreply.github.com>
2023-09-02 21:12:44 -0400

    experiment

--- demo.js
+++ demo.js
@@ -98,7 +98,7 @@ loader.load( './assets/models/decor/decorC1 render quality.glb', ( gltf ) => {
                 const videoMaterial = new THREE.MeshStandardMaterial({
                     color: 0x0,
                     emissive: 0xffffff,
-                    emissiveMap: tloader.load('./assets/textures/walnut.jpg', (t)=>{ t.colorSpace = THREE.SRGBColorSpace; }),
+                    emissiveMap: tloader.load('./assets/textures/walnut.jpg'),
                     side: THREE.FrontSide,
                     emissiveIntensity: 1.2,
                     toneMapped: true,
@@ -115,12 +115,12 @@ loader.load( './assets/models/decor/decorC1 render quality.glb', ( gltf ) => {
 
             if (child.name == "Bounce_Light"){
                 child.material = new THREE.MeshBasicMaterial({
-                    map: tloader.load('./assets/textures/walnut.jpg', (t)=>{ t.colorSpace = THREE.SRGBColorSpace; }),
-                    // eloader.load('./assets/textures/lampDiffuse.exr', (t)=>{ t.colorSpace = THREE.SRGBColorSpace; }),
-                    // tloader.load('./assets/textures/walnut.jpg', (t)=>{ t.colorSpace = THREE.SRGBColorSpace; }),
+                    map: tloader.load('./assets/textures/walnut.jpg'),
+                    // eloader.load('./assets/textures/lampDiffuse.exr'),
+                    // tloader.load('./assets/textures/walnut.jpg'),
                 });
                 child.material.blending = THREE.MultiplyBlending;
-                child.material.transparent = true;
+                // child.material.transparent = true;
                 
                 console.log("Lamp Diffuse", child);
             }
@@ -128,10 +128,11 @@ loader.load( './assets/models/decor/decorC1 render quality.glb', ( gltf ) => {
             if (child.name == "Bounce_Light_Area"){
                 child.material = new THREE.MeshBasicMaterial({
                     map: tloader.load('./assets/textures/walnut.jpg'),
-                    // eloader.load('./assets/textures/vaseDiffuse.exr', (t)=>{ t.colorSpace = THREE.SRGBColorSpace; }),
+                    // eloader.load('./assets/textures/vaseDiffuse.exr'),
+                    // tloader.load('./assets/textures/walnut.jpg'),
                 });
-                child.material.blending = THREE.MultiplyBlending;
-                child.material.transparent = true;
+                // child.material.blending = THREE.MultiplyBlending;
+                // child.material.transparent = true;
                 
                 console.log("Vase Diffuse", child);
             }
