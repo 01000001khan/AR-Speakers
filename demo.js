8c8091a6881d0b1dda84091858d73966110ed9f7
Λ <87445503+01000001khan@users.noreply.github.com>
2023-09-04 17:39:07 -0400

    should be same useless

--- demo.js
+++ demo.js
@@ -50,55 +50,62 @@ new RGBELoader().load( './assets/textures/leadenhall.hdr', function ( texture ) 
     scene.environment = texture;
 });
 
-// const vs = `
-// varying vec2 vUv;
+const vs = `
+varying vec2 vUv;
 
-// void main() {
-//     vUv = uv;
-//     vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-//     gl_Position = projectionMatrix * modelViewPosition;
-// }
-// `
+void main() {
+    vUv = uv;
+    vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * modelViewPosition;
+}
+`
 
-// const fs = `
-// uniform sampler2D tex;
-// uniform float light;
-// varying vec2 vUv;
+const fs = `
+uniform sampler2D tex;
+uniform float light;
+varying vec2 vUv;
 
-// void main() {
-//     vec2 rUv = vUv;
-//     rUv.y = rUv.y * -1. + 1.; // Seems backwards
-//     gl_FragColor = (texture2D(tex, rUv)*light + 1.) / (light + 1.);
-// }
-// `
+void main() {
+    vec2 rUv = vUv;
+    rUv.y = rUv.y * -1. + 1.; // Seems backwards
+    gl_FragColor = (texture2D(tex, rUv)*light + 1.) / (light + 1.);
+}
+`
 
-// const lampu = {
-//     tex: {
-//         value: tloader.load('./assets/textures/lampDiffuse.png')
-//     }
-// };
+const lampu = {
+    tex: {
+        value: tloader.load('./assets/textures/lampDiffuse.png')
+    }
+};
 
-// const vaseu = {
-//     tex: {
-//         value: tloader.load('./assets/textures/vaseDiffuse.png')
-//     }
-// };
+const vaseu = {
+    tex: {
+        value: tloader.load('./assets/textures/vaseDiffuse.png')
+    }
+};
 
-// let lampLight = new THREE.ShaderMaterial({
-//     uniforms: lampu,
-//     vertexShader: vs,
-//     fragmentShader: fs,
-//     blending: THREE.MultiplyBlending,
-//     transparent: true
-// });
+let lampLight = new THREE.ShaderMaterial({
+    uniforms: lampu,
+    vertexShader: vs,
+    fragmentShader: fs,
+    transparent = true,
+    blending = THREE.CustomBlending, 
+    blendEquation = THREE.AddEquation,
+    blendSrc = THREE.DstColorFactor,
+    blendDst = THREE.OneFactor,
+    
+});
 
-// let vaseLight = new THREE.ShaderMaterial({
-//     uniforms: vaseu,
-//     vertexShader: vs,
-//     fragmentShader: fs,
-//     blending: THREE.MultiplyBlending,
-//     transparent: true
-// });
+let vaseLight = new THREE.ShaderMaterial({
+    uniforms: vaseu,
+    vertexShader: vs,
+    fragmentShader: fs,
+    transparent = true,
+    blending = THREE.CustomBlending, 
+    blendEquation = THREE.AddEquation,
+    blendSrc = THREE.DstColorFactor,
+    blendDst = THREE.OneFactor,
+});
 
 
 
@@ -167,32 +174,12 @@ loader.load( './assets/models/decor/decorC1 render quality.glb', ( gltf ) => {
             }
 
             if (child.name == "Bounce_Light"){
-                // child.material = lampLight;
-
-                child.material = new THREE.MeshBasicMaterial({
-                    map: tloader.load('./assets/textures/lampDiffuse.png'),
-                });
-                child.material.transparent = true;
-                child.material.blending = THREE.CustomBlending; 
-                child.material.blendEquation = THREE.AddEquation;
-                child.material.blendSrc = THREE.DstColorFactor;
-                child.material.blendDst = THREE.OneFactor;
-
+                child.material = lampLight;
                 console.log("Lamp Diffuse", child);
             }
             
             if (child.name == "Bounce_Light_Area"){
-                // child.material = vaseLight;
-
-                child.material = new THREE.MeshBasicMaterial({
-                    map: tloader.load('./assets/textures/vaseDiffuse.png'),
-                });
-                child.material.transparent = true;
-                child.material.blending = THREE.CustomBlending; 
-                child.material.blendEquation = THREE.AddEquation;
-                child.material.blendSrc = THREE.DstColorFactor;
-                child.material.blendDst = THREE.OneFactor;
-
+                child.material = vaseLight;
                 console.log("Vase Diffuse", child);
             }
             
